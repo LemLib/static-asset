6620b7da5a5747528d1920eae993b80d523f4476
Liam Teale <111480281+SizzinSeal@users.noreply.github.com>
2024-05-19 14:58:30 -0700

    upgrade to pros 4, remove gif-pros

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,5 @@
 #include "main.h"
-#include "asset.h"
-#include "gif-pros/gifclass.hpp"
-
-ASSET(chip_gif)
 
 void initialize() { }
 
-void opcontrol() {
-	Gif gif(chip_gif, lv_scr_act());
-
-	while (true) {
-		pros::delay(20);
-	}
-}
+void opcontrol() {}
